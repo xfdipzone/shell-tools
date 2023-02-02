ced8a477ce2a48a3259c8b18bcfa0076a6331aa3
xfdipzone <xfdipzone@gmail.com>
2023-02-02 12:48:32 +0800

    style: update indent

--- monitor-kill-expire-process/monitor_kill_expire_process.sh
+++ monitor-kill-expire-process/monitor_kill_expire_process.sh
@@ -12,29 +12,29 @@ set -o errexit
 # 进程运行时间格式化为秒数
 function get_etime_seconds(){
 
-	# 参数：运行时间
-	local runtime=$1
+    # 参数：运行时间
+    local runtime=$1
 
-	# 判断是否含有d (exp:4d06)
-	if [[ $runtime =~ "d" ]]; then
-		seconds=$(echo $runtime|awk -F 'd' '{print $1*3600*24 + $2*3600}')
-		echo $seconds
-		return 0
-	fi
+    # 判断是否含有d (exp:4d06)
+    if [[ $runtime =~ "d" ]]; then
+        seconds=$(echo $runtime|awk -F 'd' '{print $1*3600*24 + $2*3600}')
+        echo $seconds
+        return 0
+    fi
 
-	# 判断是否含有h (exp:1h40)
-	if [[ $runtime =~ "h" ]]; then
-		seconds=$(echo $runtime|awk -F 'h' '{print $1*3600 + $2*60}')
-		echo $seconds
-		return 0
-	fi
+    # 判断是否含有h (exp:1h40)
+    if [[ $runtime =~ "h" ]]; then
+        seconds=$(echo $runtime|awk -F 'h' '{print $1*3600 + $2*60}')
+        echo $seconds
+        return 0
+    fi
 
-	# 判断是否含有: (exp:22:38)
-	if [[ $runtime =~ ":" ]]; then
-		seconds=$(echo $runtime|awk -F ':' '{print $1*60 + $2}')
-		echo $seconds
-		return 0
-	fi
+    # 判断是否含有: (exp:22:38)
+    if [[ $runtime =~ ":" ]]; then
+        seconds=$(echo $runtime|awk -F ':' '{print $1*60 + $2}')
+        echo $seconds
+        return 0
+    fi
 
 }
 
@@ -44,41 +44,41 @@ readonly process_config=('mytest.php,120')
 # 循环监控进程
 for config in ${process_config[*]}; do
 
-	# 获取进程关键字及超时时间
-	process=$(echo $config|awk -F ',' '{print $1}')
-	expire=$(echo $config|awk -F ',' '{print $2}')
+    # 获取进程关键字及超时时间
+    process=$(echo $config|awk -F ',' '{print $1}')
+    expire=$(echo $config|awk -F ',' '{print $2}')
 
-	# 获取进程id
-	process_id=$(ps -ef | grep "$process" | grep -v "grep" | awk '{print $1}')
+    # 获取进程id
+    process_id=$(ps -ef | grep "$process" | grep -v "grep" | awk '{print $1}')
 
-	if [[ ! -z "$process_id" ]]; then
+    if [[ ! -z "$process_id" ]]; then
 
-		# 获取进程运行时间
-		process_runtime=$(ps -eo pid,etime|grep " $process_id "|awk -F ' ' '{print $2}')
+        # 获取进程运行时间
+        process_runtime=$(ps -eo pid,etime|grep " $process_id "|awk -F ' ' '{print $2}')
 
-		if [[ -z "$process_runtime" ]]; then
+        if [[ -z "$process_runtime" ]]; then
 
-			# 获取进程运行时间
-			process_runtime=$(ps -eo pid,etime|grep "$process_id "|awk -F ' ' '{print $2}')
+            # 获取进程运行时间
+            process_runtime=$(ps -eo pid,etime|grep "$process_id "|awk -F ' ' '{print $2}')
 
-		fi
+        fi
 
-		# 将进程运行时间转为秒数
-		process_runtime_second=$(get_etime_seconds $process_runtime)
+        # 将进程运行时间转为秒数
+        process_runtime_second=$(get_etime_seconds $process_runtime)
 
-		echo "process PID:${process_id} ${process} runtime:${process_runtime} seconds:${process_runtime_second}s"
+        echo "process PID:${process_id} ${process} runtime:${process_runtime} seconds:${process_runtime_second}s"
 
-		# 判断是否超时
-		if [[ "$process_runtime_second" -ge "$expire" ]]; then
+        # 判断是否超时
+        if [[ "$process_runtime_second" -ge "$expire" ]]; then
 
-			echo "kill PID:${process_id} ${process} runtime>=${expire}s"
+            echo "kill PID:${process_id} ${process} runtime>=${expire}s"
 
-			# 终止进程
-			kill -9 $process_id
+            # 终止进程
+            kill -9 $process_id
 
-		fi
+        fi
 
-	fi
+    fi
 
 done
 
